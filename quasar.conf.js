7d0502a736626d50d8d07f2694ac7d16824458a0
Mihai Turcu <mihai.turcu@cctech.ai>
2022-07-03 16:07:16 +0300

    Added new API key

--- quasar.conf.js
+++ quasar.conf.js
@@ -53,7 +53,7 @@ module.exports = configure(function (ctx) {
             vueRouterMode: 'hash', // available values: 'hash', 'history'
 
             env: {
-                BREADWINNER_API_KEY: '80e0925d-ac5c-486a-b18a-4a50759fd873', // for localhost domain, in production it would be for beavers.com
+                BREADWINNER_API_KEY: 'bc32c16f-e63f-413f-a3d6-661ff1ce7219', // for localhost domain, in production it would be for beavers.com
             },
 
             // transpile: false,
